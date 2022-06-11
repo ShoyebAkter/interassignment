e7a5c487ac34362bd440f0056a8640878240623e
Shoyeb180 <shoyebmohammad660@gmail.com>
2022-06-12 00:28:55 +0600

    problem with two array

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,17 +2,36 @@ import React, { useState } from 'react';
 import { useQuery, gql } from "@apollo/client";
 import './Shop.css';
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const PRODUCTS_QUERY = gql`
 query{
-    product(id:"jacket-canada-goosee"){
+    product(id:"huarache-x-stussy-le"){
       name,
+      inStock,
       gallery,
+      description,
+      category,
+      brand,
       prices{
+        currency{
+          label,
+          symbol
+        },
         amount
+      },
+      attributes{
+        id,
+        name,
+        type,
+        items{
+          displayValue,
+          value
+        }
       }
     }
-  }
+  
+}
 `;
 const Shop = () => {
     const [cart, setCart] = useState([]);
@@ -21,27 +40,27 @@ const Shop = () => {
      if (loading) return "Loading...";
     if (error) return <pre>{error.message}</pre>
 
-    const handleAddToCart = (selectedProduct) =>{
-        console.log(selectedProduct);
-        // let newCart = [];
-        // const exists = cart.find(product => product.id === selectedProduct.id);
-        // if(!exists){
-        //     selectedProduct.quantity = 1;
-        //     newCart = [...cart, selectedProduct];
-        // }
-        // else{
-        //     const rest = cart.filter(product => product.id !== selectedProduct.id);
-        //     exists.quantity = exists.quantity + 1;
-        //     newCart = [...rest, exists];
-        // }
+    // const handleAddToCart = (selectedProduct) =>{
+    //     console.log(selectedProduct);
+    //     let newCart = [];
+    //     const exists = cart.find(product => product.id === selectedProduct.id);
+    //     if(!exists){
+    //         selectedProduct.quantity = 1;
+    //         newCart = [...cart, selectedProduct];
+    //     }
+    //     else{
+    //         const rest = cart.filter(product => product.id !== selectedProduct.id);
+    //         exists.quantity = exists.quantity + 1;
+    //         newCart = [...rest, exists];
+    //     }
         
-        // setCart(newCart);
-        // addToDb(selectedProduct.id);
-    }
+    //     setCart(newCart);
+    //     // addToDb(selectedProduct.id);
+    // }
 
     return (
         <div className='shop-container'>
-            <div className="products-container">
+            {/* <div className="products-container">
                 {
                    data.product.gallery.map(image=><Product 
                         image={image}
@@ -50,7 +69,11 @@ const Shop = () => {
                         ></Product>)
                 }
             </div>
-            
+            <div className="cart-container">
+                <Cart cart={cart}>
+                    
+                </Cart>
+            </div> */}
         </div>
     );
 };
